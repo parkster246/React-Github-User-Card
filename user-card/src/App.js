ca61c5ed5fe264d56214941c6113354a929631b2
Parker williams <parkster246@gmail.com>
2020-08-04 13:48:09 -0500

    near MVP

---
+++ user-card/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios'
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      user: {},
+      followers: []
+    };
+  };
+  componentDidMount() {
+this.getGithubUser();
+this.getGithubFollowers();
+  }
+  getGithubUser = async () => {
+    let res = await axios.get('https://api.github.com/users/parkster246')
+    this.setState({
+      user: res.data
+    });
+  };
+  getGithubFollowers = async () => {
+    let res = await axios.get('https://api.github.com/users/parkster246/followers')
+    this.setState({
+      followers: res.data
+    });
+  };
+  render() {
+    const { user, followers } = this.state;
+    return 
+  }
+};
+
+export default App;
